270c870867e2795a4b4b21f36f14413bc049b5d0
gabsdou <153685240+gabsdou@users.noreply.github.com>
2024-05-15 09:14:02 +0200

    Update Operateur.java

--- Operateur.java
+++ Operateur.java
@@ -1,4 +1,19 @@
 public abstract class Operateur{
+    public int evaluer(int operande1){
+        if operande1.getClass() == Operateur.class {
+            return this.evaluer(operande1.evaluer());
+        }
+        else if(operande1.getClass()==int){
+            return operande1;
+        }
+    }
+
+//analyser caractère par caractère la phrase  
+//Si c'est un chiffre, c'est une constante numérique
+//Si c'est des lettres, faut lire jusqu'a la parenthese ouvrante,
+// En gros faut lire jusqu'a une parenthese ouvrante, fermante ou virgule
+// Construire un arbre, avec des noeuds
+    
     static public int and(int a, int b){
         return a & b;
     }
