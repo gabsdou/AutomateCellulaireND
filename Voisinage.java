178cae68904f2b9340903859b845f8054679c31d
Tismey <108024662+Tismey@users.noreply.github.com>
2024-06-15 18:53:37 +0200

    declutter voisinage

--- Voisinage.java
+++ Voisinage.java
@@ -1,32 +1,13 @@
 public class Voisinage{
     private int nbVoisin;
-    private int dimension;
     private int[][] n_plet;
-    private int iteration;
     public static int[] position;
 
     public Voisinage(int nbVoisin, int dimension,int[][] n_plet){
         this.nbVoisin = nbVoisin;
-        iteration = 0;
         this.n_plet = n_plet;
     }
-    public boolean hasNext(){
-        if(iteration < nbVoisin){
-            return true;
-        }
-        resetIterator();
-        return false;
-    }
-    public int[] next(){
-        if(!hasNext()){
-            return null;
-        }
-        iteration++;
-        return n_plet[iteration - 1];
-    }
-    public void resetIterator(){
-        iteration = 0;
-    }
+    
     public int[][] getVoisinage(){
         return n_plet;
     }
